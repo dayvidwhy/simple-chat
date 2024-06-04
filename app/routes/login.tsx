4bdf1ed67d2abc178db8f2d1162d0fc7ee495718
dayvidwhy <dayvidwhy@gmail.com>
2024-06-04 16:06:48 +1000

    Replace deprecated icon.

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,5 +1,5 @@
 import { Form } from "@remix-run/react";
-import { Github } from "lucide-react";
+import { LogIn } from "lucide-react";
 
 export default function Login() {
     return (
@@ -7,7 +7,7 @@ export default function Login() {
             <Form className="flex justify-center" action="/auth/github" method="post">
                 <button
                     className="flex border rounded border-slate-400 p-4 hover:bg-slate-200">
-                    <Github />Login with GitHub
+                    <LogIn className="mr-1" />Login with GitHub
                 </button>
             </Form>
         </div>
